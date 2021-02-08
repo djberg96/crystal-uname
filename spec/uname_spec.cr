ede42050279bf6d2eede7f0f9c420e933a011f40
Daniel Berger <djberg96@gmail.com>
2021-02-08 10:36:49 -0500

    Add specs for model method.

--- spec/uname_spec.cr
+++ spec/uname_spec.cr
@@ -26,6 +26,15 @@ describe System do
     System.version.should eq(expected)
   end
 
+  it "correctly reports the model on Darwin or raises an error if not supported" do
+    {% if flag?(:darwin) %}
+      expected = `sysctl hw.model`.split(":").last.strip
+      System.model.should eq(expected)
+    {% else %}
+      expect_raises(RuntimeError, "the model method is unsupported on this platform")
+    {% end %}
+  end
+
   it "returns a struct if the uname method is used" do
     System.uname.should be_a(System::Uname)
   end
